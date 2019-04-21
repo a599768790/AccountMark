73363372dbd2022bdd01659d2957d4aca12c5985
599768790@qq.com <599768790@qq.com>
2019-04-21 23:14:10 +0800

    04212314

--- js/rem.js
+++ js/rem.js
@@ -8,17 +8,32 @@
 // })(window,document);
 
 
-fnResize()
-window.onresize = function () {
-fnResize()
-}
-function fnResize() {
-var deviceWidth = document.documentElement.clientWidth || window.innerWidth
-if (deviceWidth >= 750) {
-	deviceWidth = 750
-}
-if (deviceWidth <= 320) {
-	deviceWidth = 320
-}
-document.documentElement.style.fontSize = (deviceWidth / 7.5) + 'px'
-}
+// fnResize()
+// window.onresize = function () {
+// fnResize()
+// }
+// function fnResize() {
+// var deviceWidth = document.documentElement.clientWidth || window.innerWidth
+// if (deviceWidth >= 750) {
+// 	deviceWidth = 750
+// }
+// if (deviceWidth <= 320) {
+// 	deviceWidth = 320
+// }
+// document.documentElement.style.fontSize = (deviceWidth / 7.5) + 'px'
+// }
+
+
+// rem 1:100解决方案
+(function (doc, win) {
+            var docEl = doc.documentElement,
+                resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+                recalc = function () {
+                    var clientWidth = docEl.clientWidth;
+                    if (!clientWidth) return;
+                    docEl.style.fontSize = (clientWidth / 3.75) + 'px';
+                };
+            if (!doc.addEventListener) return;
+            win.addEventListener(resizeEvt, recalc, false);
+            doc.addEventListener('DOMContentLoaded', recalc, false);
+        })(document, window);
